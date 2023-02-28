5a5a5baabafdd10ad60dd6e224fbd579cf3d57a4
Bhagyapatel0914 <124074223+Bhagyapatel0914@users.noreply.github.com>
2023-02-28 16:31:21 +0530

    Create Arrays.java

---
+++ Arrays & ArrayList/Arrays.java
@@ -0,0 +1,16 @@
+// Arrays are collection of datatypes
+// Syntax --> datatype [] variable = new datatype[size];
+// also datatype [] variable = {};
+public class arrays{
+    public static void main(String []args){
+         //        int [] arr = new int[5];
+//        arr[0] = 1 ;
+//        arr [1] = 2;
+//        arr[2]= 3;
+//        arr [3]= 4;
+//        arr[4] = 5;
+//        System.out.println(arr[0]);
+        int [] arr = {2, 3 ,4 , 5, 78};
+      System.out.println(arr[0]);
+    }
+}
